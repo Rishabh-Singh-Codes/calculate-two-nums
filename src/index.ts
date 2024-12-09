e501860aa6c868d7a232c60055b08629ee8f44ec
Rishabh Singh <rishabhsinghcodes@gmail.com>
2024-12-09 18:55:28 +0530

    published npm package

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+export const addTwoNumbers = (num1: number, num2: number) => {
+  return num1 + num2;
+};
+
+export const subtractTwoNumbers = (num1: number, num2: number) => {
+  return num1 - num2;
+};
+
+export const multiplyTwoNumbers = (num1: number, num2: number) => {
+  return num1 * num2;
+};
+
+export const divideTwoNumbers = (num1: number, num2: number) => {
+  return num1 / num2;
+};
+
+export const modTwoNumbers = (num1: number, num2: number) => {
+  return num1 % num2;
+};
